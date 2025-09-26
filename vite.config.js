ac0eeb459fec367aaa3b668db8e8a7fa895fad42
TioaChan <8370450+TioaChan@users.noreply.github.com>
2025-09-26 15:49:20 +0800

    refactor: update package.json scripts and add auto-import functionality

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
 
 export default defineConfig({
-  plugins: [vue()],
-  base: './',
-  build: {
-    outDir: 'src/renderer/dist',
-    emptyOutDir: true
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src/renderer')
-    }
-  },
-  server: {
-    port: 5173,
-    host: '0.0.0.0'
-  }
-})
+    plugins: [
+        vue(),
+        AutoImport({
+            imports: ["vue"],
+            resolvers: [ElementPlusResolver()],
+            dts: "src/renderer/types/auto-import.d.ts",
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+            dts: "src/renderer/types/components.d.ts",
+        }),
+    ],
+    base: "./",
+    build: {
+        outDir: "src/renderer/dist",
+        emptyOutDir: true,
+    },
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src/renderer"),
+        },
+    },
+    server: {
+        port: 5173,
+        host: "0.0.0.0",
+    },
+});
